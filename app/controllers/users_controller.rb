e702ad0a81422318f9eb99b8ed76695c333e222e
lekheshwarb <lekheshwar.bejjarapu@freshworks.com>
2022-09-04 15:12:29 +0530

    Add User Controller

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,32 @@
+class UsersController < ApplicationController
+  def index
+    users = User.all()
+    render json: { status: "SUCCESS", message: "Loaded users", data: users }, status: :ok
+  end
+
+  def create
+    user = User.new(user_params)
+
+    if user.save
+      render json: { status: "SUCCESS", message: "Added User", data: user }, status: :ok
+    else
+      render json: { status: "ERROR", message: "User Not Saved", data: user }, status: :unprocessable_entity
+    end
+  end
+
+  def update
+    user = User.find(params[:id])
+
+    if user.update(user_params)
+      render json: { status: "SUCCESS", message: "Updated User", data: user }, status: :ok
+    else
+      render json: { status: "SUCCESS", message: "Failed to Update User", data: user }, status: :ok
+    end
+  end
+
+  private
+
+  def user_params
+    params.permit(:name, :role, :email, :phone, :address)
+  end
+end
